3a418fc8e208c69ab3dd90b499ee13c6327238b3
Samarjit Baro <xamarbro10@gmail.com>
2024-12-10 14:41:56 -0800

    middleware

--- express.js
+++ express.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
+app.use(function (req, res, next) {
+  console.log("middleware");
+  next();
+});
+
 app.get("/about", function (req, res) {
   res.send("about Page");
 });
