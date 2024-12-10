901466e588fa945df0bce46906e4d776553731d4
Samarjit Baro <xamarbro10@gmail.com>
2024-12-10 15:05:42 -0800

    session

--- express.js
+++ express.js
@@ -1,19 +1,38 @@
 const express = require("express");
 const app = express();
-
+const expressSession = require("express-session");
 app.use(function (req, res, next) {
   console.log("middleware");
   next();
 });
+app.use(
+  expressSession({
+    secret: "kuch bhi",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.get("/create", function (req, res, next) {
+  req.session.polo = true;
+  res.send("done");
+  next();
+});
+app.get("/check", function (req, res, next) {
+  console.log(req.session.polo);
+  next();
+});
 
-app.get("/about", function (req, res) {
+app.get("/about", function (req, res, next) {
   res.send("about Page");
+  next();
 });
-app.get("/profile", function (req, res) {
+app.get("/profile", function (req, res, next) {
   res.send("Profile Page");
+  next();
 });
-app.get("*", function (req, res) {
+app.get("*", function (req, res, next) {
   res.send("Error, Site doesnt exist");
+  next();
 });
 
 app.listen(3000);
